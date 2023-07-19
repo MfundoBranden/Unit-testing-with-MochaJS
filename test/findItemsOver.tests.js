24a35267fea8fc2b3020224d535d315f9abfd9dd
MfundoBranden <132061751+MfundoBranden@users.noreply.github.com>
2023-07-19 13:35:44 +0200

    Update findItemsOver.tests.js

--- test/findItemsOver.tests.js
+++ test/findItemsOver.tests.js
@@ -1,10 +1,45 @@
-describe('Test Find items over function', function () {
+var itemList = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+    {name : 'apples', qty : 3},
+];
 
-    it('it should return products that quantity higher than the threshold.', function () {
-        var result = [
-            {name : 'pears', qty : 37},
-            {name : 'bananas', qty : 27},
-        ];
+var results = [
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+];
+
+var itemList2 = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 19},
+    {name : 'bananas', qty : 17},
+    {name : 'apples', qty : 3},
+];
+
+var results2 = [];
+
+var itemList3 = [
+    {name : 'apples', qty : 40},
+    {name : 'pears', qty : 20},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+];
+
+var results3 = [
+    {name : 'apples', qty : 40},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+];
+
+describe('The findItemsOver20 function', function(){ 
+    it('This should return the item list1 with over 20', function(){
+        assert.deepEqual(results, findItemsOver20(itemList));
     });
-
+    it('This should return the item list2 with over 20', function(){
+        assert.deepEqual(results2, findItemsOver20(itemList2));
+    });
+    it('This should return the item list3 with over 20', function(){
+        assert.deepEqual(results3, findItemsOver20(itemList3));
+    });  
 });
