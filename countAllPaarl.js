7943dd76fa54596c1252c5dcfd38776457ffcb80
MfundoBranden <132061751+MfundoBranden@users.noreply.github.com>
2023-07-20 13:09:31 +0200

    Update countAllPaarl.js

--- countAllPaarl.js
+++ countAllPaarl.js
@@ -1,14 +1,16 @@
-function countAllPaarl (reg) {
-    let Count = 0;
-    
-    let regArr = reg.split(",");
-    for (let v in  regArr){
-        let trimmed = regArr [v].trim();
-        if (trimmed.startsWith("CJ")){
-            Count+= 1;
-        }
+var regNumbers = 'CL 900, CJ 678 543, CA 34567, CJ 67890, CN 7864';
+
+var regNumbersForPaarl = allPaarl(regNumbers);
+function allPaarl(regNo){
+    var reg = regNo.split(",");
+    var allPaarl = [];
+    for(let i = 0;i<reg.length;i++){
+      let fromPaarl=reg[i].trim();
+      if(fromPaarl.startsWith("CJ")){
+        allPaarl.push(fromPaarl);
+      }
     }
-    return Count;
-}
-
-
+   return allPaarl;
+   console.log(allPaarl); 
+  }
+  console.log(allPaarl("CL 900, CJ 678 543, CA 34567, CJ 67890, CN 7864"))
